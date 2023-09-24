435e9efc63e01ff8e66f6b961ad66e7587385f9c
Zaid Ahmed <zaidahmed2345@gmail.com>
2023-09-24 02:01:15 -0700

    init

--- js/home.js
+++ js/home.js
@@ -1,3 +1,23 @@
+// Favorite button
+let favouriteIcons = document.querySelectorAll(".fa-heart");
+
+favouriteIcons.forEach((icon) => {
+  const numberElement = icon.nextElementSibling; // Get the <p> element next to the icon
+  let isFavourite = false;
+
+  icon.addEventListener("click", () => {
+    isFavourite = !isFavourite; // Toggle the favorite state
+    icon.classList.toggle("favourite-checked");
+
+    // Update the number based on the favorite state
+    if (isFavourite) {
+      numberElement.textContent = parseInt(numberElement.textContent) + 1;
+    } else {
+      numberElement.textContent = parseInt(numberElement.textContent) - 1;
+    }
+  });
+});
+
 // Our Teams Swiper
 
 let swiper = new Swiper(".ourteams__cards", {
